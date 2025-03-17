f36c18ddf6d1da314567f5fb1dac0fc9e03ab8ca
HPNam@Home <hoangphu.nam0604@gmail.com>
2025-03-17 23:15:05 +0700

    trả về ngoiaj lệ json cho api

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -24,5 +24,48 @@ return Application::configure(basePath: dirname(__DIR__))
     ]);
   })
   ->withExceptions(function (Exceptions $exceptions) {
-   
+
+    // Xử lý lỗi API chung
+    $exceptions->render(function (\Throwable $e, $request) {
+      if ($request->is('api/*')) {
+        $statusCode = $e instanceof \Symfony\Component\HttpKernel\Exception\HttpException
+          ? $e->getStatusCode()
+          : 500;
+
+        $message = $e->getMessage();
+        if ($e instanceof \Illuminate\Validation\ValidationException) {
+          return response()->json([
+            'success' => false,
+            'error' => $message,
+            'messages' => $e->errors(),
+            'code' => 422
+          ], 422);
+        }
+
+        // Danh sách mã lỗi và thông điệp tương ứng
+        $errorResponses = [
+          \Illuminate\Auth\AuthenticationException::class => ['Unauthorized', 401],
+          \Illuminate\Auth\Access\AuthorizationException::class => ['Forbidden', 403],
+          \Symfony\Component\HttpKernel\Exception\NotFoundHttpException::class => ['Not Found', 404],
+          \Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException::class => ['Method Not Allowed', 405],
+          \Illuminate\Validation\ValidationException::class => ['Validation failed', 422],
+          \Illuminate\Http\Exceptions\ThrottleRequestsException::class => ['Too Many Requests', 429]
+        ];
+        foreach ($errorResponses as $exceptionClass => [$errorMessage, $code]) {
+          if ($e instanceof $exceptionClass) {
+            return response()->json([
+              'success' => false,
+              'error' => $errorMessage,
+              'code' => $code
+            ], $code);
+          }
+        }
+
+        return response()->json([
+          'success' => false,
+          'error' => $message,
+          'code' => $statusCode
+        ], $statusCode);
+      }
+    });
   })->create();
