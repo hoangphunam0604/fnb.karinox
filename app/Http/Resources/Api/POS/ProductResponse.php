eadd77be8b020a8ffe666c7d6cc3aeda03e8ccd2
HPNam@Home <hoangphu.nam0604@gmail.com>
2025-03-18 00:16:32 +0700

    thêm thumbnail cho sản phẩm

--- app/Http/Resources/Api/POS/ProductResponse.php
+++ app/Http/Resources/Api/POS/ProductResponse.php
@@ -20,7 +20,7 @@ class ProductResponse extends JsonResource
       'code' => $this->code,
       'name' => $this->name,
       'price' => $this->price,
-      'allows_sale' => $this->allows_sale,
+      'thumbnail' => $this->thumbnail,
       'is_reward_point' => $this->is_reward_point,
       'product_type' => $this->product_type,
       'toppings' => ProductToppingResponse::collection($this->whenLoaded('toppings')),
