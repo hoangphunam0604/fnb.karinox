d7dad1fc7c5134d3844199f5eef11ad42274a3e5
Hoàng Phú Nam <hoangphu.nam0604@gmail.com>
2025-06-26 17:42:50 +0700

    up

--- routes/api.php
+++ routes/api.php
@@ -7,10 +7,9 @@ use App\Http\Controllers\Api\POS\Payments\VNPayQRController;
 
 Route::post('/pos/payments/vnpayqr/ipn', [VNPayQRController::class, 'ipn']);
 Route::get('branches', [BranchController::class, 'getUserBranches']);
+Route::post('branches/select', [BranchController::class, 'selectBranch']);
+require __DIR__ . '/api-auth.php';
 Route::middleware(['auth:api', 'is_karinox_app', 'set_karinox_branch_id'])->group(function () {
-  Route::post('branches/select', [BranchController::class, 'selectBranch']);
-  require __DIR__ . '/api-auth.php';
   require __DIR__ . '/api-pos.php';
-  require __DIR__ . '/api-kitchen.php';
   require __DIR__ . '/api-admin.php';
 });
