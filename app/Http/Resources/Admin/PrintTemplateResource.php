fa92c88b40adcd9ad849e7481535bc54e505de32
HPNam@Home <hoangphu.nam0604@gmail.com>
2025-09-08 00:53:41 +0700

    a

--- app/Http/Resources/Admin/PrintTemplateResource.php
+++ app/Http/Resources/Admin/PrintTemplateResource.php
@@ -22,7 +22,7 @@ class PrintTemplateResource extends JsonResource
       'content'     => $this->content,
       'is_default'  => $this->is_default,
       'is_active'   => $this->is_active,
-      'updated_at'  => $this->updated_at->toDateTimeString(),
+      'updated_at'  => $this->updated_at->format("H:i d/m/Y"),
     ];
   }
 }
