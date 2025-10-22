af89729a331b92bd15398ca48d736f2ecc48e8ea
HPNam@Home <hoangphu.nam0604@gmail.com>
2025-10-23 00:26:58 +0700

    up

--- app/Http/Print/Requests/ConnectRequest.php
+++ app/Http/Print/Requests/ConnectRequest.php
@@ -14,15 +14,14 @@ class ConnectRequest extends FormRequest
   public function rules(): array
   {
     return [
-      'connection_code' => 'required|string|size:12'
+      'connection_code' => 'required|string'
     ];
   }
 
   public function messages(): array
   {
     return [
-      'connection_code.required' => 'Mã kết nối là bắt buộc',
-      'connection_code.size' => 'Mã kết nối phải có đúng 12 ký tự'
+      'connection_code.required' => 'Mã kết nối là bắt buộc'
     ];
   }
 }
